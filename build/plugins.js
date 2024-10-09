e7f3dad1ccf36e73ee71f8f1da9ec7931f53d435
灰太狼的梦想 <630514215@qq.com>
2024-10-10 00:59:56 +0800

    feat: 初始化

---
+++ build/plugins.js
@@ -0,0 +1,38 @@
+import { cdn } from "./cdn";
+import react from "@vitejs/plugin-react";
+import { viteBuildInfo } from "./info";
+import svgLoader from "vite-svg-loader";
+import Inspector from "vite-plugin-react-inspector";
+import { configCompressPlugin } from "./compress";
+import removeNoMatch from "vite-plugin-router-warn";
+import { visualizer } from "rollup-plugin-visualizer";
+import removeConsole from "vite-plugin-remove-console";
+// import { vitePluginFakeServer } from "vite-plugin-fake-server";
+
+export function getPluginsList(
+  VITE_CDN,
+  VITE_COMPRESSION
+) {
+  const lifecycle = process.env.npm_lifecycle_event;
+  return [
+    react(),
+    // 按下Command(⌘)+Shift(⇧)，然后点击页面元素会自动打开本地IDE并跳转到对应的代码位置
+    Inspector(),
+    viteBuildInfo(),
+    /**
+     * 开发环境下移除非必要的vue-router动态路由警告No match found for location with path
+     * 非必要具体看 https://github.com/vuejs/router/issues/521 和 https://github.com/vuejs/router/issues/359
+     * vite-plugin-router-warn只在开发环境下启用，只处理vue-router文件并且只在服务启动或重启时运行一次，性能消耗可忽略不计
+     */
+    removeNoMatch(),
+    svgLoader(),
+    VITE_CDN ? cdn : null,
+    configCompressPlugin(VITE_COMPRESSION),
+    // 线上环境删除console
+    removeConsole({ external: ["src/assets/iconfont/iconfont.js"] }),
+    // 打包分析
+    lifecycle === "report"
+      ? visualizer({ open: true, brotliSize: true, filename: "report.html" })
+      : null
+  ];
+}
