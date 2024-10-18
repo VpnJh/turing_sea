d548a888e6c46a172f719dc32393d1613d368373
灰太狼的梦想 <630514215@qq.com>
2024-10-18 21:43:56 +0800

    feat: 添加了redux并持续化存储

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import "@/style/index.css";
 import App from "@/layout/App";
 import { I18nextProvider } from "react-i18next";
 import i18n from "@/locales/i18n.jsx";
+import * as ReactRedux from "react-redux";
+import { persistor, store } from "@/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 // 获取浏览器语言
 const lang = localStorage.getItem("lang");
 const browserLanguage = lang || navigator.language || navigator.userLanguage;
@@ -12,7 +16,11 @@ i18n.changeLanguage(browserLanguage);
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>
+  <ReactRedux.Provider store={store}>
+    <PersistGate persistor={persistor} loading={"Loading..."}>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </PersistGate>
+  </ReactRedux.Provider>
 );
